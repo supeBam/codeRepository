57c958719179c53b138a3b6b621c5605c9b795b0
snowFox <846032492@qq.com>
2025-03-12 19:02:15 +0800

    add: 刷题

--- src/main/java/org/example/leecode/Le209.java
+++ src/main/java/org/example/leecode/Le209.java
@@ -1,4 +1,35 @@
 package org.example.leecode;
 
+/**
+ * https://leetcode.cn/problems/minimum-size-subarray-sum/
+ */
 public class Le209 {
+
+    public static void main(String[] args) {
+        Le209 le209 = new Le209();
+        System.out.println(le209.minSubArrayLen(4, new int[]{1, 4, 4}));
+    }
+
+    public int minSubArrayLen(int target, int[] nums) {
+        int ans = Integer.MAX_VALUE;
+        int sum = 0;
+        int l = 0;
+        int n = nums.length;
+        for (int r = 0; r < n; r++){
+            sum += nums[r];
+            // while(sum - nums[l] >= target){
+            //     sum -= nums[l];
+            //     l++;
+            // }
+            // if(sum >= target){
+            //     ans = Math.min(ans, r - l + 1);
+            // }
+            while(sum >= target){
+                ans = Math.min(ans, r - l + 1);
+                sum -= nums[l];
+                l++;
+            }
+        }
+        return ans <= n ? ans : 0;
+    }
 }
