5b1a07daa2d6b33752f73d58232151c26df9973a
snowFox <846032492@qq.com>
2025-03-12 19:18:24 +0800

    add: 滑动窗口练习

---
+++ src/main/java/org/example/leecode/Le713.java
@@ -0,0 +1,31 @@
+package org.example.leecode;
+
+/**
+ * https://leetcode.cn/problems/subarray-product-less-than-k/description/
+ * 滑动窗口练习
+ */
+public class Le713 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k < 1){
+            return 0;
+        }
+        int ans = 0;
+        int c = 1;
+        int l = 0;
+        int n = nums.length;
+        for (int r = 0; r < n; r++){
+            c *= nums[r];
+            while(c >= k){
+                c /= nums[l];
+                l++;
+            }
+            ans += r - l + 1;
+        }
+        return ans;
+    }
+}
