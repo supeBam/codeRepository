3e8ef55ba3d0979bbb057aaf94a5fab4da3c33be
snowFox <846032492@qq.com>
2025-03-15 08:13:07 +0800

    Leetcode每日题

---
+++ src/main/java/org/example/leecode/Le3110.java
@@ -0,0 +1,12 @@
+package org.example.leecode;
+
+public class Le3110 {
+
+    public int scoreOfString(String s) {
+        int ans = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            ans += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return ans;
+    }
+}
