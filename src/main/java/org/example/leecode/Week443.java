08ee9dd123f36f01a0f7c6d1705fb7e5fb7f6679
snowFox <846032492@qq.com>
2025-03-31 18:09:18 +0800

    feat: Lc week443

---
+++ src/main/java/org/example/leecode/Week443.java
@@ -0,0 +1,30 @@
+package org.example.leecode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Week443 {
+    public static void main(String[] args) {
+        //int[] nums = new int[]{5,3,4,1,3,2};
+        //int[] nums = new int[]{1, 2, 4, 6, 7};
+
+        String s = "b", t = "aaaa";
+        //String s = "abc", t = "def";
+        //System.out.println(Arrays.toString(new Week443().minCosts(nums)));
+    }
+
+    public int[] minCosts(int[] cost) {
+        int n = cost.length;
+        int[] ListA = new int[n];
+        ListA[0] = cost[0];
+        int max = ListA[0];
+        for (int i = 1; i < n; i++) {
+            // 比较最大值
+            max = Math.min(max, cost[i]);
+            ListA[i] = max;
+        }
+        return ListA;
+    }
+
+}
