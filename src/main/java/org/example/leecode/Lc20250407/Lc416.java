eeee9a975dedb6398135a63d7f079120ed4134d6
snowFox <846032492@qq.com>
2025-04-08 00:06:20 +0800

    feat: Lc

--- src/main/java/org/example/leecode/Lc20250407/Lc416.java
+++ src/main/java/org/example/leecode/Lc20250407/Lc416.java
@@ -6,16 +6,33 @@ import java.util.*;
 //https://leetcode.cn/problems/partition-equal-subset-sum/?envType=daily-question&envId=2025-04-07
 public class Lc416 {
 
-    Set<Integer> set = new HashSet<Integer>();
-
     public boolean canPartition(int[] nums) {
         int sum = Arrays.stream(nums).sum();
         if (sum % 2 != 0) {
             return false;
         }
-        int access = sum / 2;
+        int j = sum / 2;
+        int n = nums.length;
 
-        return true;
+        // 记忆化
+        int[][] memo = new int[n][sum / 2 + 1];
+        for (int[] ints : memo) {
+            Arrays.fill(ints, -1);
+        }
+        return dfs(n - 1, j, nums, memo);
+    }
+
+    public boolean dfs(int i, int j, int[] nums, int[][] memo) {
+        if (i < 0) {
+            return j == 0;
+        }
+        if (memo[i][j] != -1) {
+            return memo[i][j] == 1;
+        }
+
+        boolean res = j >= nums[i] && dfs(i - 1, j - nums[i], nums, memo) || dfs(i - 1, j, nums, memo);
+        memo[i][j] = res ? 1 : 0;
+        return res;
     }
 
 
