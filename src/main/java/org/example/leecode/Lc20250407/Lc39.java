a956c98efaa89d6de425c96ab5baa54afd7e1761
snowFox <846032492@qq.com>
2025-04-16 11:52:17 +0800

    feat: lc回溯

--- src/main/java/org/example/leecode/Lc20250407/Lc39.java
+++ src/main/java/org/example/leecode/Lc20250407/Lc39.java
@@ -21,31 +21,52 @@ public class Lc39 {
         this.candidates = candidates;
         this.n = candidates.length;
         Arrays.sort(candidates);
-        dfs(0, 0);
+        dfs(n - 1, 0);
         return ans;
     }
 
     public void dfs(int i, int t) {
         // 还差多少
         int d = target - t;
-        // 剪枝，如果 d < 0则返回(超标了)，如果 i 前面的总和 < d， 则返回
-        if (d < 0 || i >= n) {
-            return;
-        }
         // 边界条件
         if (d == 0) {
             ans.add(new ArrayList<>(path));
-            path.clear();
+            return;
+        }
+        // 剪枝，如果 d < 0则返回(超标了)，如果 i 前面的总和 < d， 则返回
+        if (d < 0 || i >= n) {
             return;
         }
 
         int candidate = candidates[i];
         // 选的下则选自己
-        if (d >= candidate){
-            path.add(candidate);
-            dfs(i, t + candidate);
-        }
+        path.add(candidate);
+        dfs(i, t + candidate);
+        path.remove(path.size() - 1);
         // 选不下选下一个
         dfs(i + 1, t);
+    }
+
+    // 优化，利用了排序后的性质
+    public void dfs2(int i, int t) {
+        // 还差多少
+        int d = target - t;
+        // 边界条件
+        if (d == 0) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        // 剪枝，如果 d < 0则返回(超标了)，如果 i 前面的总和 < d， 则返回
+        if (d < 0 || i < 0) {
+            return;
+        }
+
+        int candidate = candidates[i];
+        // 选的下则选自己
+        path.add(candidate);
+        dfs(i, t + candidate);
+        path.remove(path.size() - 1);
+        // 选不下选下一个
+        dfs(i - 1, t);
     }
 }
