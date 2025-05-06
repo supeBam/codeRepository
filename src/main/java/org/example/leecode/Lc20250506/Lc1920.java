ac5ced2a9fb230cf8ac9388be05a48e78bfe78a4
snowFox <846032492@qq.com>
2025-05-06 10:00:43 +0800

    feat: 模拟

---
+++ src/main/java/org/example/leecode/Lc20250506/Lc1920.java
@@ -0,0 +1,13 @@
+package org.example.leecode.Lc20250506;
+
+public class Lc1920 {
+
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
